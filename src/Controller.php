37dde7da4bae957d5a8f4ab0cf64fca6fb4a88e4
Cloudmanic Labs, LLC <github@cloudmanic.com>
2014-06-18 23:07:22 -0700

    Getting started

---
+++ src/Controller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Cloudmanic\LaravelApi;
+
+class Controller extends \Controller //extends \Cloudmanic\System\Controllers\Account
+{
+	public function index()
+	{
+		return 'ff';
+	}
+}
+
+/* End File */
